3eb9c58fc0392ea9bd02f360ff050e69ec846710
Abdullah Daud <chelahmy@gmail.com>
2019-11-25 17:10:29 +0800

    Initial page component

---
+++ src/js/page.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloMessage from './hello'
+
+class Page extends React.Component {
+  render() {
+    return (
+      <div class="grid-container">
+        <div class="grid-x grid-margin-x">
+          <div class="large-12 cell">
+            <h1>Let's Synch</h1>
+          </div>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Page>
+    <HelloMessage name='Taylor'/>
+  </Page>,
+  document.getElementById('page')
+);
